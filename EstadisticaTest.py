cf0cc722e4d21d21d2452fa6a6e4b5c84bef2064
Lina <lina.m8a@gmail.com>
2016-03-13 18:43:24 -0500

    Prueba 5: Numero de elementos, mínimo + cadena vacía - (Rojo)

--- EstadisticaTest.py
+++ EstadisticaTest.py
@@ -4,7 +4,7 @@ from Estadistica import Estadistica
 
 class EstadisticaTest(TestCase):
     def testCalcularEstadisticaSinElementos(self):
-        self.assertEqual(Estadistica().calcularEstadistica(''), [0], 'Numero de elementos + cadena vacia')
+        self.assertEqual(Estadistica().calcularEstadistica(''), [0,0], 'Numero de elementos, minimo + cadena vacia')
 
     def testCalcularEstadisticaUnElemento(self):
         self.assertEqual(Estadistica().calcularEstadistica('8'), [1], 'Numero de elementos + un elemento (8)')
