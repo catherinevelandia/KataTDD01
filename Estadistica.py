85c1ea602159e17cedeb8767d311032ae7e44b96
Lina <lina.m8a@gmail.com>
2016-03-13 18:53:57 -0500

    Prueba 7: Numero de elementos, mínimo + dos elementos - (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -3,9 +3,18 @@ class Estadistica:
     def calcularEstadistica(self, cadena):
         if cadena == '':
             return [0, 0];
+
         elif "," in cadena:
             arreglo = cadena.split(",")
-            return [len(arreglo)];
+
+            if len(arreglo) == 2:
+                if arreglo[0] <= arreglo[1]:
+                    return [len(arreglo), int(arreglo[0])]
+                else:
+                    return [len(arreglo), int(arreglo[1])]
+            else:
+                return [len(arreglo)]
+
         else:
             return [1, int(cadena)];
 
