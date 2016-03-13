a38a2f4ba8d24f836754dc4cd08a9a31b5e64d99
catherinevelandia <c.velandia10@uniandes.edu.co>
2016-03-13 18:29:09 -0500

    Prueba 2: Numero de elementos + dos elementos - (Rojo)

--- EstadisticaTest.py
+++ EstadisticaTest.py
@@ -8,3 +8,6 @@ class EstadisticaTest(TestCase):
 
     def testCalcularEstadisticaUnElemento(self):
         self.assertEqual(Estadistica().calcularEstadistica('8'), [1], 'Numero de elementos + un elemento (8)')
+
+    def testCalcularEstadisticaDosElemento(self):
+        self.assertEqual(Estadistica().calcularEstadistica('8,2'), [2], 'Numero de elementos + dos elementos (8,2)')
