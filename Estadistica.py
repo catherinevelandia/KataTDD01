52f1ced3a5d25f23c2fbb970cbfa3a9e500bb491
catherinevelandia <c.velandia10@uniandes.edu.co>
2016-03-13 18:26:53 -0500

    Prueba 2: Numero de elementos + un elemento - (Rojo)

--- Estadistica.py
+++ Estadistica.py
@@ -3,3 +3,5 @@ class Estadistica:
     def calcularEstadistica(self, cadena):
         if cadena == '':
             return [0];
+
+
--- EstadisticaTest.py
+++ EstadisticaTest.py
@@ -3,5 +3,8 @@ from unittest import TestCase
 from Estadistica import Estadistica
 
 class EstadisticaTest(TestCase):
-    def test_calcularEstadistica(self):
+    def testCalcularEstadisticaSinElementos(self):
         self.assertEqual(Estadistica().calcularEstadistica(''), [0], 'Numero de elementos + cadena vacia')
+
+    def testCalcularEstadisticaUnElemento(self):
+        self.assertEqual(Estadistica().calcularEstadistica('8'), [1], 'Numero de elementos + un elemento (8)')
