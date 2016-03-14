16c4e01a99d524f1d01dfe158742f7b1f435d77c
catherinevelandia <c.velandia10@uniandes.edu.co>
2016-03-13 19:06:10 -0500

    Prueba 11: Numero de elementos, mínimo, máximo + dos elementos - (Verde)

--- Estadistica.py
+++ Estadistica.py
@@ -7,12 +7,15 @@ class Estadistica:
         elif "," in cadena:
             arreglo = cadena.split(",")
             min = int(arreglo[0])
+            max = int(arreglo[0])
 
             for a in arreglo:
                 if int(a) <= min:
                     min = int(a)
+                if int(a) > max:
+                    max = int(a)
 
-            return [len(arreglo), min]
+            return [len(arreglo), min, max]
 
         else:
             return [1, int(cadena), int(cadena)];
