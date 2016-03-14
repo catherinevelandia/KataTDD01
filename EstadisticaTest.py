12a7d233a2892fa952713b5b7fbea19f3155c3b1
Lina <lina.m8a@gmail.com>
2016-03-13 19:11:24 -0500

    Prueba 15: Numero de elementos, mínimo, máximo y promedio + dos elementos - (Rojo)

--- EstadisticaTest.py
+++ EstadisticaTest.py
@@ -10,7 +10,7 @@ class EstadisticaTest(TestCase):
         self.assertEqual(Estadistica().calcularEstadistica('8'), [1, 8, 8, 8], 'Numero de elementos, minimo, maximo, promedio + un elemento (8)')
 
     def testCalcularEstadisticaDosElementos(self):
-        self.assertEqual(Estadistica().calcularEstadistica('8,2'), [2, 2, 8], 'Numero de elementos, minimo, maximo + dos elementos (8,2)')
+        self.assertEqual(Estadistica().calcularEstadistica('8,2'), [2, 2, 8, 5], 'Numero de elementos, minimo, maximo + dos elementos (8,2)')
 
     def testCalcularEstadisticaVariosElementos(self):
         self.assertEqual(Estadistica().calcularEstadistica('8,2,3,7,9'), [5, 2, 9], 'Numero de elementos, minimo, maximo + cinco elementos (8,2,3,7,9)')
